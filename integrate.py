eac18d997808b84c80fce5d87e35714c2255750b
sykokorev <skokorev1981@gmail.com>
2023-11-11 12:54:45 +0300

    Added ODE IVP solver integrate.solve_ivp

--- integrate.py
+++ integrate.py
@@ -8,7 +8,7 @@ def rk45(y, fun, yn, t, h):
     return s1 + s2
 
 
-def solve_ipv(fun, tspan, y0, method='RK45', teval=None, args=()):
+def solve_ipv(fun, tspan, y0, method='RK45', args=()):
     '''
         Solve an initial value problem for ODE
         --------------------------------------
@@ -28,23 +28,24 @@ def solve_ipv(fun, tspan, y0, method='RK45', teval=None, args=()):
                             Integration method to use:
                                 *   'RK45' (default): Explicit Runge-Kutta method of
                                     order 4
-                        teval: array_like or None, optional
-                            Times at which to store the computed solution, must be lie
-                            within tspan. If None (default), use points selected by solver
         Rerturns:       t: ndarray, shape(n_points)
                         y: ndarray, shape(n, n_points)
                             
     '''
 
-    yf = y0
+    th = (tspan[1] - tspan[0]) / 100
+    tsol = [tspan[0] + i * th for i in range(101)]
+
     n = len(y0)
-    tf = tspan[0]
-    tsol = [tf]
-    ysol = [[y0i] for y0i in yf]
-    teval = 100 if not teval else teval
-    h = (tspan[1] - tspan[0]) / teval
-    
-    for s in range(teval):
+    m = len(tsol)
+    ysol = [[0.0 for i in range(m)] for j in range(n)]
+    for i in range(n):
+        ysol[i][0] = y0[i]
+
+    for i in range(1, len(tsol)):
+        h = tsol[i] - tsol[i - 1]
+        tf = tsol[i - 1]
+        yf = [yi[i - 1] for yi in ysol]
         k1 = fun(tf, yf, *args)
         k2 = fun(tf + h / 2, [yfi + h * k1i / 2 for yfi, k1i in zip(yf, k1)], *args)
         k3 = fun(tf + h / 2, [yfi + h * k2i / 2 for yfi, k2i in zip(yf, k2)], *args)
@@ -53,10 +54,9 @@ def solve_ipv(fun, tspan, y0, method='RK45', teval=None, args=()):
             yfi + (h / 6) * (k1i + 2 * k2i + 2 * k3i + k4i) 
             for yfi, k1i, k2i, k3i, k4i in zip(yf, k1, k2, k3, k4)
         ]
-        tf += h
-        tsol.append(tf)
-        for i in range(n):
-            ysol[i].append(yf[i])
+
+        for j in range(n):
+            ysol[j][i] = yf[j]
     
     return tsol, ysol
 
