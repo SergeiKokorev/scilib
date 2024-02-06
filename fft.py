d33cc5c1bb17ed72f16c7b046f9a7a3969a35ab8
Кокорев Сергей Юрьевич <SKok@uwca.ru>
2024-02-06 15:28:47 +0300

    Finished class FFT. Added classmethods shift (to shift the zero frequency component to the center of spectrum) and freq (returns the Discrete Fourier Transform sample frequencies)

--- fft.py
+++ fft.py
@@ -3,35 +3,124 @@ from __future__ import annotations
 import math
 
 from typing import List
-from dataclasses import dataclass
 
 
-@dataclass
+def fft1(xin: List(float | complex | ComplexNumber)) -> List[ComplexNumber | complex | float]:
+
+    '''
+    Performs the Fast Fourier Transform (FFT) for a given signal.
+    =============================================================
+
+    Parameters:
+    -----------
+
+    xin:     array_like
+        List of complex numbers. The input signal to be transformed.
+    
+    Returns:
+    --------
+
+    tr_signal:  array_like.
+        The list of complex numbers. The transformed signal.
+    
+    '''
+
+    n = len(xin)
+    
+    if n % 2 != 0:
+        raise RuntimeError(f'Algorithm fft1 assumes N is a power of 2')
+
+    xout = [0.0 for i in range(n)]
+
+    for k in range(n//2):
+
+        even = sum([xi * ComplexNumber.exp(-2 * math.pi * k * m / (n // 2))
+        for m, xi in enumerate(xin[0::2])])
+        odd = sum([xi * ComplexNumber.exp(-2 * math.pi * k * m / (n // 2))
+        for m, xi in enumerate(xin[1::2])])
+
+        rate = ComplexNumber.exp(-2 * math.pi * k / n)
+
+        xout[k] = even + rate * odd
+        xout[k + n//2] = even - rate * odd
+
+    return xout
+
+
 class FFT:
 
-    signal: List[ComplexNumber]
-    frequency: List[ComplexNumber] = None
-    phase: list = None
-    amplitude: list = None
-    real: list = None
-    imag: list = None
+    def __init__(self, signal: List[ComplexNumber | complex | float]):
 
-    def compute_fft(self):
-        self.frequency = fft1(self.signal)
-        self.amplitude = [(cn.real ** 2 + cn.imag ** 2) for cn in self.frequency]
-        self.phase = [math.atan2(cn.imag, cn.real) for cn in self.frequency]
-        self.real = [cn.real for cn in self.frequency]
-        self.imag = [cn.imag for cn in self.frequency]
+        self.__signal = signal
+        self.__frequency = fft1(self.signal)
+
+    @property
+    def signal(self):
+        return self.__signal
+
+    @signal.setter
+    def signal(self, signal: List[ComplexNumber | complex | float]):
+        if not hasattr(signal, '__iter__'):
+            raise TypeError('Unsupported variable type for signal')
+        elif not all([isinstance(s, complex | ComplexNumber | float | int) for s in signal]):
+            raise TypeError('Unsupported variable type for signal. Signal must be List[float | int | ComplexNumber | complex]')
+        self.__signal = signal
+        self.__frequency = fft1(self.signal)
+
+    @property
+    def frequency(self):
+        return self.__frequency
+    
+    @property
+    def real(self):
+        return [f.real for f in self.frequency]
+    
+    @property
+    def imag(self):
+        return [f.imag for f in self.frequency]
+    
+    @property
+    def phase(self):
+        return [math.atan2(cn.imag, cn.real) for cn in self.frequency]
+    
+    @property
+    def amplitute(self):
+        return [(cn.real ** 2 + cn.imag ** 2) ** 0.5 for cn in self.frequency]
+
+    @classmethod
+    def freq(cls, n: int, d: float = 1.0) -> List[float]:
+
+        f = [0.0 for i in range(n)]
+        if n % 2 == 0:
+            for i in range(n):
+                f[i] = i if i <= (n / 2 - 1) else (i - n)
+                f[i] /= (d * n)
+        else:
+            for i in range(n):
+                f[i] = i if i <= (n - 1) / 2 else i - n
+                f[i] /= (d * n)
+
+        return f
+    
+    @classmethod
+    def shift(cls, freqs: List[float]) -> List[float]:
+
+        n = len(freqs)
+        fs = [0.0 for i in range(n)]
+        if n % 2 == 0:
+            for i, k in enumerate(range(int(n / 2), n)):
+                fs[i] = freqs[k]
+            for i in range(int(n / 2)):
+                fs[int(n / 2 + i)] = freqs[i]
+        else:
+            for i, k in enumerate(range(int((n - 1) / 2 + 1), n)):
+                fs[i] = freqs[k]
+            for i in range(int((n - 1) / 2 + 1)):
+                fs[int((n - 1) / 2 + i)] = freqs[i]
+        return fs
 
     def __round__(self, ndigits=0):
-        out = '[ '
-        for i, f in enumerate(self.frequency):
-            if i % 2 == 0 and i != 0:
-                out += f'\n{round(f, ndigits)}\t'
-            else:
-                out += f'{round(f, ndigits)}\t'
-        out += ' ]'
-        return out
+        return [round(f, ndigits) for f in self.frequency]
 
     def __str__(self):
         out = '[ '
@@ -123,8 +212,8 @@ class ComplexNumber:
         other = self.__transform__(other)
         devider = other.real ** 2 + other.imag ** 2
         return ComplexNumber(
-            (self.real * other.real + self.imag * other.imag) / divider,
-            (self.imag * other.real - self.real * other.imag) / divider
+            (self.real * other.real + self.imag * other.imag) / devider,
+            (self.imag * other.real - self.real * other.imag) / devider
         )
     
     def __rtruediv__(self, other):
@@ -140,56 +229,10 @@ class ComplexNumber:
         return self.__truediv__(other)
 
     def __round__(self, ndigits=0):
-        if self.imag >= 0:
-            return f"{round(self.real, ndigits)} + {round(self.imag, ndigits)}j"
-        else:
-            return f"{round(self.real, ndigits)} - {abs(round(self.imag, ndigits))}j"
+        return ComplexNumber(round(self.real, ndigits), round(self.imag, ndigits))
 
     def __str__(self):
         if self.imag >= 0:
             return f"{self.real} + {self.imag}j"
         else:
             return f"{self.real} - {abs(self.imag)}j"
-
-def fft1(xin: List(float | complex | ComplexNumber)) -> List(ComplexNumber):
-
-    '''
-    Performs the Fast Fourier Transform (FFT) for a given signal.
-    =============================================================
-
-    Parameters:
-    -----------
-
-    xin:     array_like
-        List of complex numbers. The input signal to be transformed.
-    
-    Returns:
-    --------
-
-    tr_signal:  array_like.
-        The list of complex numbers. The transformed signal.
-    
-    '''
-
-    n = len(xin)
-    
-    if n % 2 != 0:
-        raise RuntimeError(f'Algorithm fft1 assumes N is a power of 2')
-
-    xout = [0.0 for i in range(n)]
-
-    for k in range(n//2):
-
-        even = sum([xi * ComplexNumber.exp(-2 * math.pi * k * m / (n // 2))
-        for m, xi in enumerate(xin[0::2])])
-        odd = sum([xi * ComplexNumber.exp(-2 * math.pi * k * m / (n // 2))
-        for m, xi in enumerate(xin[1::2])])
-
-        rate = ComplexNumber.exp(-2 * math.pi * k / n)
-
-        xout[k] = even + rate * odd
-        xout[k + n//2] = even - rate * odd
-
-    return xout
-
-
--- test/test_fft.py
+++ test/test_fft.py
@@ -14,7 +14,7 @@ DIR = os.path.abspath(os.path.join(
 ))
 sys.path.append(DIR)
 
-from fft import fft1, ComplexNumber, FFT
+from fft import FFT as fft1
 
 SAMPLE_RATE = 44100
 DURATION = 5
@@ -30,28 +30,44 @@ def generate_sine_wave(freq, sample_rate, duration):
 
 if __name__ == "__main__":
 
-    # N = 600
-    # T = 1.0 / 800.0
-    # x = np.linspace(0.0, N * T, endpoint=False)
-    # y = np.sin(50.0 * 2.0 * np.pi * x) + 0.5 * np.sin(80.0 * 2.0 * np.pi * x)
+    N = 600
+    T = 1.0 / 800.0
+    x = np.linspace(0.0, N * T, endpoint=False)
+    y = np.sin(50.0 * 2.0 * np.pi * x) + 0.5 * np.sin(80.0 * 2.0 * np.pi * x)
 
-    # yf = fft(y)
-    # ym = fft1(y)
+    yf = fft(y)
+    ym = fft1(y)
     
-    # for k, (y1, y2) in enumerate(zip(yf, ym)):
-    #     print(f'{k}\t:\t{np.round(y1, 4)}\t:\t{round(y2, 4)}')
+    for k, (y1, y2) in enumerate(zip(yf, ym.frequency)):
+        print(f'{k}\t:\t{np.round(y1, 4)}\t:\t{round(y2, 4)}')
 
-    # signal = np.array([-2, 8, 6, 4, 1, 0, 3, 5])
+    signal = np.array([-2, 8, 6, 4, 1, 0, 3, 5])
     signal = np.arange(256)
 
     sp = fft(np.sin(signal))
     freq = fftfreq(signal.shape[-1])
 
-    sp1 = FFT(np.sin(signal))
-    sp1.compute_fft()
+    sp1 = fft1(np.sin(signal))
+    freq1 = sp1.freq(signal.shape[-1])
 
     plt.plot(freq, sp.real, freq, sp.imag)
     plt.show()
-    plt.plot(freq, sp1.real, freq, sp1.imag)
+    plt.plot(freq1, sp1.real, freq1, sp1.imag)
     plt.show()
 
+    signal = np.array([-2, 8, 6, 4, 1, 0, 3, 5, 8], dtype=float)
+    fourier = fft(signal)
+    n = signal.size
+    timestep = 0.1
+    freq = fftfreq(n, d=timestep)
+    print(freq)
+
+    freq_my = fft1.freq(n, timestep)
+    print(freq_my)
+
+    freqs = fftfreq(10, 0.1)
+    freqs_my = fft1.freq(10, 0.1)
+    print(np.round(freqs, 3))
+    print([round(f, 3) for f in freqs_my])
+    print(np.round(fftshift(freqs), 3))
+    print([round(f, 3) for f in fft1.shift(freqs_my)])
